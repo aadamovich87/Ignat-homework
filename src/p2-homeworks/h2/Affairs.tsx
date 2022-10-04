91c2eeef6cb727fd093ac763c645886acf646cfd
Aliaksei Adamovich <lifelida@yandex.ru>
2022-10-04 17:00:29 +0300

    first commit

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[]
+    setFilter:(filter:FilterType)=>void
+    deleteAffairCallback:(_id: number)=>void
 }
 
 function Affairs(props: AffairsPropsType) {
@@ -17,20 +17,28 @@ function Affairs(props: AffairsPropsType) {
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = (filter:FilterType) => {
+        props.setFilter('all')
+    } // need to fix
+    const setHigh = (filter:FilterType) => {
+        props.setFilter('high')
+    }
+    const setMiddle = (filter:FilterType) => {
+        props.setFilter('middle')
+    }
+    const setLow = (filter:FilterType) => {
+        props.setFilter('low')
+           }
 
     return (
         <div>
 
             {mappedAffairs}
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
+            <button onClick={()=>setAll('all')}>All</button>
+            <button onClick={()=>setHigh('high')}>High</button>
+            <button onClick={()=>setMiddle('middle')}>Middle</button>
+            <button onClick={()=>setLow('low')}>Low</button>
         </div>
     )
 }
